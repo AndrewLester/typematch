4f7f667f95841ec1355f346c098829af3de72d2a
AndrewLester <alester220@gmail.com>
2022-03-01 23:10:18 -0600

    Add site title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -143,6 +143,9 @@ $: sectionViewEnd = done
       );
 </script>
 
+<svelte:head>
+    <title>TypeMatch</title>
+</svelte:head>
 <a href="https://github.com/AndrewLester/typematch" class="github">
     <svg width="32" height="32" viewBox="0 0 32 32" fill="none">
         <path
