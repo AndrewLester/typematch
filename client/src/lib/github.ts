72e1262daa14579d6dfefd511bb0c6fdd97dd77e
AndrewLester <alester220@gmail.com>
2022-11-05 20:07:08 -0500

    Remove github.ts

--- client/src/lib/github.ts
+++
@@ -1,9 +0,0 @@
-import { Octokit } from '@octokit/rest';
-
-const github = new Octokit({
-    auth: import.meta.env.VITE_GITHUB_ACCESS_TOKEN,
-    userAgent: 'TypeRacer v1.0',
-    timeZone: 'US/Central',
-});
-
-export default github;
