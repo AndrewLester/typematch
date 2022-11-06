5212d6e69c65cc2db8da4daa0fc41664f103353e
AndrewLester <alester220@gmail.com>
2022-11-06 01:58:33 -0500

    Revert changes

--- client/src/routes/game/[code]/+page.ts
+++ client/src/routes/game/[code]/+page.ts
@@ -1,41 +1,24 @@
-import { GameState, type MultiplayerGame, type User } from '$lib/types';
-import type { Load } from '@sveltejs/kit';
-import { PUBLIC_WORKER_HOST } from '$env/static/public';
 import { dev } from '$app/environment';
+import { PUBLIC_WORKER_HOST } from '$env/static/public';
+import type { MultiplayerGame, User } from '$lib/types';
+import type { Load } from '@sveltejs/kit';
 
 export const load: Load = async ({ params, fetch }) => {
-    // const [game, me] = (await Promise.all([
-    //     fetch(
-    //         `http${!dev ? 's' : ''}://${PUBLIC_WORKER_HOST}/game/${
-    //             params.code
-    //         }`,
-    //     ).then((res) => res.json()),
-    //     fetch(
-    //         `http${!dev ? 's' : ''}://${PUBLIC_WORKER_HOST}/game/${
-    //             params.code
-    //         }/me`,
-    //         {
-    //             credentials: 'include',
-    //         },
-    //     ).then((res) => (res.status < 300 ? res.json() : null)),
-    // ])) as [MultiplayerGame, User];
-
-    const me: User = {
-        admin: true,
-        connected: true,
-        country: 'US',
-        id: '5235',
-        name: 'test',
-        ping: 5,
-        position: 0,
-    };
-
-    const game: MultiplayerGame = {
-        state: GameState.Waiting,
-        users: { [me.id]: me },
-        passageIndex: 0,
-        startTime: 0,
-    };
+    const [game, me] = (await Promise.all([
+        fetch(
+            `http${!dev ? 's' : ''}://${PUBLIC_WORKER_HOST}/game/${
+                params.code
+            }`,
+        ).then((res) => res.json()),
+        fetch(
+            `http${!dev ? 's' : ''}://${PUBLIC_WORKER_HOST}/game/${
+                params.code
+            }/me`,
+            {
+                credentials: 'include',
+            },
+        ).then((res) => (res.status < 300 ? res.json() : null)),
+    ])) as [MultiplayerGame, User];
 
     return {
         game,
